cc1882fa47770f2fd71499e702a9eb75183280cc
pnk-85 <pnkghogare27@gmail.com>
2023-12-28 15:10:16 +0530

    added helper

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const express = require('express');
 
 const app = express();
@@ -15,7 +17,7 @@ app.use(shopRouts);
 
 
 app.use((req,res,next) => {
-    res.status(404).send('<h1>page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,13 @@
+const path = require('path')
+
 const express = require("express");
+
+const rootDir = require('../util/helper')
 
 const router = express.Router();
 
 router.get('/add-product',(req, res) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">add<form>');
+    res.sendFile(path.join(rootDir,'views', 'add-product.html'))
 });
 
 router.post('/add-product', (req,res)=> {
